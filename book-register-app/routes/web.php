fe65cad93b36925e041f29c1c49bf643ca9ce313
Jonas Magalhães <info.jonas@gmail.com>
2025-06-05 16:47:15 -0300

    update routes

--- book-register-app/routes/web.php
+++ book-register-app/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\AssuntoController;
+use App\Http\Controllers\AutorController;
+use App\Http\Controllers\LivroController;
+use App\Http\Controllers\RelatorioLivrosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->name('dashboard');
+
+Route::resources([
+    'livros' => LivroController::class,
+    'autores' => AutorController::class,
+    'assuntos' => AssuntoController::class,
+]);
+
+Route::get('/relatorio/livros', [RelatorioLivrosController::class, 'tela'])
+    ->name('relatorio.livros.tela');
+Route::get('/relatorio/livros/pdf', [RelatorioLivrosController::class, 'pdf'])
+    ->name('relatorio.livros.pdf');
